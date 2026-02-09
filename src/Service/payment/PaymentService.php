03e199666291db631ee67545142101100d8d6ccb
Lalison <lalison.randrianantenaina@gmail.com>
2026-02-09 13:52:36 +0300

    modification bug

--- src/Service/payment/PaymentService.php
+++ src/Service/payment/PaymentService.php
@@ -26,8 +26,11 @@ class PaymentService
     }
     public function insertPayment(UtilisateurEntity $utilisateur, Etudiants $etudiant, Niveaux $niveau, Payments $payment, $typeDroit): Payments
     {
-        if ($payment->getMontant() <= 0) {
-            throw new Exception('Le montant ne doit pas être inférieur ou égal à 0 ' . $payment->getMontant());
+        if ($payment->getMontant()==0) {
+            return $payment;
+        }
+        if ($payment->getMontant() < 0) {
+            throw new Exception('Le montant ne doit pas être inférieur à 0 ' . $payment->getMontant());
         }
         $payment->setUtilisateur($utilisateur);
         $typeDroitEntity = $this->typeDroitsService->getById($typeDroit);
